3315ba0da89d0c23acb0311cca7e7b0998e030f5
Gaurav Puri <gauravpuri1011@gmail.com>
2021-11-09 15:28:07 +0530

    Apply Distinct packet identifiers to each outgoing message. Packet Identifiers could be reused after 65536 messages been delivered bcoz Packet identifier field is short.

--- src/Services/MqttPublishQos1DispatchService.cs
+++ src/Services/MqttPublishQos1DispatchService.cs
@@ -3,17 +3,26 @@ using MqttLibNet.Packets;
 using MqttLibNet.Packets.Data;
 using MqttLibNet.Packets.Handlers;
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace MqttLibNet.Services
 {
+    /// <summary>
+    /// This workflow uses a DistinctUntilChanged kind of mechanism 
+    /// prevIdentifierReceived will be the identifier last received acknowledgement for
+    /// there may be a chance that some retry has happened and so some dup packets been sent to broker
+    /// but we will need only one ack to start pushing new message
+    /// runningIdentifier is what helps to distinguish between the packets.
+    /// </summary>
     public class MqttPublishQos1DispatchService : IObserver<(byte[] Data, byte Flag)>
     {
         private readonly MqttStreamReaderWriter mqttStreamReaderWriter;
-        private SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
+        private SemaphoreSlim semaphoreSlim = new SemaphoreSlim(0, 1);
         private SemaphoreSlim _lock = new SemaphoreSlim(1,1);
+        private short runningIdentifier;
+        private short prevIdentifierReceived;
+        private int counter;
         public MqttPublishQos1DispatchService(MqttStreamReaderWriter mqttStreamReaderWriter)
         {
             this.mqttStreamReaderWriter = mqttStreamReaderWriter;
@@ -33,16 +42,20 @@ namespace MqttLibNet.Services
         {
             PubAck pubAck = new PubAck();
             var data = pubAck.Deserialize(pubAckBytes.Data);
-            if(data.PacketIdentifier == 1)
+            if(prevIdentifierReceived != data.PacketIdentifier &&
+                data.PacketIdentifier == runningIdentifier)
             {
+                Console.WriteLine($"Ack for packet {data.PacketIdentifier}");
+                prevIdentifierReceived = runningIdentifier;
                 semaphoreSlim.Release();
             }
-            Console.WriteLine($"Ack for packet {data.PacketIdentifier}");
         }
 
         public async Task PublishAsync(PublishData publishData)
         {
             await _lock.WaitAsync();
+            runningIdentifier = (short)(Interlocked.Increment(ref counter) % 65536);
+            publishData.PacketIdentifier = runningIdentifier;
             await PublishInternal(publishData);
             while (!await semaphoreSlim.WaitAsync(10000))
             {
@@ -56,7 +69,6 @@ namespace MqttLibNet.Services
         {
             PublishQos1 publishQos1 = new PublishQos1();
             publishData.QosLevel = QosLevel.Qos1;
-            publishData.PacketIdentifier = 1;
             var qos1Bytes = publishQos1.Serialize(publishData);
             await mqttStreamReaderWriter.WriteAsync(qos1Bytes);
         }
